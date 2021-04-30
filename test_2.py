ecb35dabbd5298befe93f33ce14c0d017110c2fe
Debora <deboribo@gmail.com>
2021-04-29 19:01:19 -0700

    finish structred-english

---
+++ test_2.py
@@ -0,0 +1,9 @@
+"""
+start
+open file 'Final.txt'
+count number of grades in the file
+find the average by adding up all the grades and dividing them by the total number of grades
+find the percentage of grades that are above the average grade
+output the results
+end
+"""
